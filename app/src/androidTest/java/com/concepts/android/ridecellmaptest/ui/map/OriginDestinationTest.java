1a54faceb9d7660a8064961fd63d7bdac3a6c435
siris <sirisgadson@gmail.com>
2017-03-14 11:14:14 -0400

    fixed expresso tests

--- app/src/androidTest/java/com/concepts/android/ridecellmaptest/ui/map/OriginDestinationTest.java
+++ app/src/androidTest/java/com/concepts/android/ridecellmaptest/ui/map/OriginDestinationTest.java
@@ -1,4 +1,4 @@
-package com.concepts.android.ridecellmaptest.ui.input;
+package com.concepts.android.ridecellmaptest.ui.map;
 
 
 import android.support.test.espresso.ViewInteraction;
@@ -7,13 +7,13 @@ import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
 import com.concepts.android.ridecellmaptest.R;
+import com.concepts.android.ridecellmaptest.ui.input.MainActivity;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.replaceText;
@@ -24,38 +24,31 @@ import static org.hamcrest.Matchers.allOf;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class MainActivityTest {
+public class OriginDestinationTest {
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void mainActivityTest() {
+    public void originDestinationTest() {
         ViewInteraction appCompatEditText = onView(
                 allOf(withId(R.id.main_origin), isDisplayed()));
-        appCompatEditText.perform(click());
+        appCompatEditText.perform(replaceText("33.751650, -84.401388"), closeSoftKeyboard());
 
         ViewInteraction appCompatEditText2 = onView(
-                allOf(withId(R.id.main_origin), isDisplayed()));
-        appCompatEditText2.perform(replaceText("33.751650, -84.401388"), closeSoftKeyboard());
+                allOf(withId(R.id.main_destination), isDisplayed()));
+        appCompatEditText2.perform(replaceText("33.952358, -84.551987"), closeSoftKeyboard());
 
         ViewInteraction appCompatEditText3 = onView(
-                allOf(withId(R.id.main_destination), isDisplayed()));
-        appCompatEditText3.perform(replaceText("33.952358, -84.551987"), closeSoftKeyboard());
-
-        ViewInteraction appCompatEditText4 = onView(
                 allOf(withId(R.id.main_speed), isDisplayed()));
-        appCompatEditText4.perform(replaceText("200"), closeSoftKeyboard());
+        appCompatEditText3.perform(replaceText("500"), closeSoftKeyboard());
 
         ViewInteraction appCompatButton = onView(
                 allOf(withId(R.id.main_btn), withText("Go"), isDisplayed()));
         appCompatButton.perform(click());
 
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(5000);
+            Thread.sleep(30000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
